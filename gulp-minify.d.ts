efa502e64c10d8203b39679af644064c791915f8
Riccardo MARRAGHINI <riccardo.marraghini@kering.com>
2024-08-01 12:00:18 +0200

    [FIX] - Add Description for parameter

--- gulp-minify.d.ts
+++ gulp-minify.d.ts
@@ -1,20 +1,24 @@
 /// <reference path="../typings/main.d.ts" />
 
 declare namespace GulpMinify {
-	interface MinifyOptions {
-		ext?: {
-			src?: string;
-			min?: string;
-		},
-		exclude?: Array<string>,
-		ignoreFiles?: Array<string>
-	}
+  interface MinifyOptions {
+    ext?: {
+      src?: string;
+      min?: string;
+    };
+    exclude?: Array<string>;
+    ignoreFiles?: Array<string>;
+    /**
+     * defines if the source files are in ES6 moudule format
+     */
+    module?: boolean;
+  }
 }
 
 declare module "gulp-minify" {
-	import { Duplex } from 'stream';
+  import { Duplex } from "stream";
 
-	function minify(options?: GulpMinify.MinifyOptions): Duplex; 
+  function minify(options?: GulpMinify.MinifyOptions): Duplex;
 
-	export = minify;
+  export = minify;
 }
